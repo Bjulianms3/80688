e6e165d55eee87ee0fc4a6a51f3ec43694e934b0
bjulianms3 <bernardojulianmarinsanchez0@gmail.com>
2023-11-23 19:19:00 -0600

    .

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,7 +1,7 @@
 package mx.uv;
 
 import static spark.Spark.*;
-
+import com.google.gson.*;
 /**
  * Hello world!
  *
@@ -11,6 +11,33 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+
+options("/*",(request,response)->{
+
+    String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+    
+    if(accessControlRequestHeaders!=null){
+    
+    response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+    
+    }
+    
+    String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+    
+    if(accessControlRequestMethod!=null){
+    
+    response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+    
+    }
+    
+    return "OK";
+    
+    });
+    
+    before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
         get("/", 
             (request, response) -> "<h1>Bienvenidx</h1>"
         );
@@ -26,6 +53,38 @@ public class App
          get("/alumno", 
             (request, response) -> "{'alumno: ','john','matricula: ','S2199399','Carrera: ','TC'}"
         );
+
+        post("/alumno",
+               (request, response) -> {
+                    System.out.println(request.contentLength());
+                    System.out.println(request.contentType());
+                    System.out.println(request.contextPath());
+
+                    return "hola: "+request.queryParams("nombre");
+               }
+        );
+
+        get("/", (req, res) -> "hola");
+
+        post("/", (req, res) -> {
+            System.out.println(req.queryParams("email") + " " +
+                               req.queryParams("password"));
+            System.out.println(req.body());
+                               res.status(200);//
+
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj", "hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+
+            return oRespuesta;
+        });
+
+        post("/", (request, response) ->{
+            JsonObject respuesta = new JsonObject();
+            respuesta.addProperty("tipo-usuario", "hola");
+            
+            return respuesta;
+        });
 
     }
 }
